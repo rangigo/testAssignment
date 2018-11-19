8e088b5656ba82d68e4a3d619b190f7221246f46
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-11-19 13:40:42 +0200

    fetch data

--- client/src/DeliverySummary.js
+++ client/src/DeliverySummary.js
@@ -1,27 +1,18 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export class DeliverySummary extends Component {
   state = {
-    ordersArray: []
+    ordersArray: [],
   }
 
   componentDidMount() {
-    fetch('https://localhost:8000/orders')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ordersArray: data})
-        console.log(this.state.ordersArray);
-        console.log('data', data);
-      })
-      .catch(err => console.log(err))
+    axios.get('http://localhost:8000/orders')
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err))
   }
 
   render() {
-    return (
-      <div>
-        
-      </div>
-    )
+    return <div />
   }
 }
 
