a2d4552582d1ad62f15e116c6f9f3f739e68b2c2
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-11-19 14:02:59 +0200

    zdfasdf

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import DeliverySummary from './DeliverySummary';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import DeliverySummary from './DeliverySummary'
 
 class App extends Component {
   render() {
     return (
-      <div class="container">
-      <h1>Orders</h1>
-      <DeliverySummary />
-    </div>
-    );
+      <div className="container">
+        <h1>Orders</h1>
+        <DeliverySummary />
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
--- client/src/DeliverySummary.js
+++ client/src/DeliverySummary.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Record from './Record'
+
+function convertDate(inputFormat) {
+  function pad(s) {
+    return s < 10 ? '0' + s : s
+  }
+  var d = new Date(inputFormat)
+  return [pad(d.getDate()), pad(d.getMonth() + 1), d.getFullYear()].join('.')
+}
 export class DeliverySummary extends Component {
   state = {
     records: [],
   }
 
+  sortDate = (a, b) => {
+    function pad(s) {
+      return s < 10 ? '0' + s : s
+    }
+    var d1 = new Date(a.Date)
+    var d2 = new Date(b.Date)
+    const diff = d1 - d2
+    return diff > 0 ? 1 : diff === 0 ? 0 : -1
+  }
+
   componentDidMount() {
     axios
       .get('http://localhost:8000/orders')
-      .then(res => this.setState({ records: res.data }))
+      .then(res =>
+        this.setState({
+          records: res.data
+            .sort(this.sortDate)
+            // .filer(el => (el.Customer ? el : null)),
+        }),
+      )
       .catch(err => console.log(err))
   }
 
@@ -17,7 +41,7 @@ export class DeliverySummary extends Component {
     const { records } = this.state
     console.log(records)
     return (
-      <table class="table">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">id</th>
@@ -29,7 +53,7 @@ export class DeliverySummary extends Component {
         </thead>
         <tbody>
           {records.map((record, i) => (
-            <Record {...record} index={i+1} />
+            <Record {...record} index={i + 1} key={record._id} />
           ))}
         </tbody>
       </table>
