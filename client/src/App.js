67de7f85168d8085a8f092e4a32bb5c85b7128d9
codefeak <codefever17@gmail.com>
2018-11-19 13:32:55 +0200

    deleted table

--- client/src/App.js
+++ client/src/App.js
@@ -9,37 +9,6 @@ class App extends Component {
       <div class="container">
       <h1>Orders</h1>
       <DeliverySummary />
-      <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">id</th>
-              <th scope="col">Customer</th>
-              <th scope="col">Date</th>
-              <th scope="col">Is Delivered</th>
-              <th scope="col">Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-              <td>the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
     </div>
     );
   }
